d3ab6a0d7bbad4913dcaf44c39ece792c11c9e6f
shaseply <vspox23@gmail.com>
2026-06-19 17:27:04 +0300

    builder refactoring;

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: path.join(__dirname, 'src/js', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
+        filename: 'assets/js/[name].[contenthash].js',
+        assetModuleFilename: 'assets/img/[name].[hash][ext]',
         clean: true,
     },
     module: {
@@ -36,10 +37,16 @@ module.exports = {
             {
                 test: /\.(?:ico|png|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[name].[hash][ext]',
+                },
             },            
             {
                 test: /\.svg$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[name].[hash][ext]',
+                },                
             },
             {
                 test: /\.(woff(2)?|eot|ttf|otf)$/i,
